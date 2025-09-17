4a8710073872259fdc4a6e5ba8c6a6e0cf8a3ab7
Mosh <mosh_shood@hotmail.com>
2025-09-17 16:41:56 +0100

    ignore terrafrom binaries and state files

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    use_azuread_auth     = true
-    storage_account_name = "ter20572"
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
-  }
-}
